bdab27f3c0a2181b711d9aee695a89c35e011560
Eldar Rizaev <eldarrizaevsw@mail.ru>
2023-01-22 20:07:00 +0100

    add

---
+++ main.py
@@ -0,0 +1,25 @@
+def task1():
+    geo_logs = [
+        {'visit1': ['Москва', 'Россия']},
+        {'visit2': ['Дели', 'Индия']},
+        {'visit3': ['Владимир', 'Россия']},
+        {'visit4': ['Лиссабон', 'Португалия']},
+        {'visit5': ['Париж', 'Франция']},
+        {'visit6': ['Лиссабон', 'Португалия']},
+        {'visit7': ['Тула', 'Россия']},
+        {'visit8': ['Тула', 'Россия']},
+        {'visit9': ['Курск', 'Россия']},
+        {'visit10': ['Архангельск', 'Россия']}
+    ]
+    return [geo_log for geo_log in geo_logs if "Россия" in next(iter(geo_log.values()))]
+
+def task2():
+    ids = {'user1': [213, 213, 213, 15, 213],
+           'user2': [54, 54, 119, 119, 119],
+           'user3': [213, 98, 98, 35]}
+    return (set(sum(ids.values(), [])))
+
+def task3():
+    stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+    return max(stats, key=stats.get)
+
